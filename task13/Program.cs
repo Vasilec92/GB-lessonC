4c8ddb14c3726fdc4d6bf9c3d5080f784a34e955
Anna Vasileva <vasilec1992@yandex.rum>
2023-08-08 11:18:18 +0200

    dz2 add

---
+++ task13/Program.cs
@@ -0,0 +1,42 @@
+﻿/* Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6 */
+
+int GetThirdDigit(int num)
+{
+
+   if (num < 100)
+   {
+      return -1;
+   }
+
+   if (num > 999)
+   {
+      return (num / 100) % 10;
+
+   }
+
+   int thirdDigit = num % 10;
+   return thirdDigit;
+}
+
+
+
+
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+int thirdDigit = GetThirdDigit(number);
+
+if (thirdDigit != -1)
+{
+   Console.WriteLine($"Третья цифра числа: {thirdDigit}");
+}
+else
+{
+   Console.WriteLine("Третьей цифры нет");
+}
+
+
---
+++ task15/Program.cs
@@ -0,0 +1,20 @@
+﻿
+/* 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным */
+
+bool IsWeekend(int dayNumber)
+{
+   return dayNumber == 6 || dayNumber == 7;
+}
+
+
+Console.Write("Введите номер дня недели (1-7): ");
+int dayNumber = int.Parse(Console.ReadLine());
+
+if (IsWeekend(dayNumber))
+{
+   Console.WriteLine("Этот день является выходным.");
+}
+else
+{
+   Console.WriteLine("Этот день не является выходным.");
+}
