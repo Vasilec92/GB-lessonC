84b35160a63735d48edad543aaf3ecd27511343b
Anna Vasileva <vasilec1992@yandex.rum>
2023-08-24 15:53:38 +0200

    dz3

---
+++ task19/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+/* Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да */
+
+bool IsPalindrome(int num)
+{
+   string numString = num.ToString();
+
+   for (int i = 0; i < numString.Length / 2; i++)
+   {
+      if (numString[i] != numString[numString.Length - 1 - i])
+      {
+         Console.WriteLine("Нет, это число не является палиндромом.");
+         return false;
+      }
+   }
+   Console.WriteLine("Да, это число является палиндромом.");
+   return true;
+}
+
+IsPalindrome(14212);
+IsPalindrome(12821);
+IsPalindrome(23432);
+
---
+++ task21/Program.cs
@@ -0,0 +1,11 @@
+﻿/* Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+A (3,6,8); B(2, 1, -7), -> 15.84
+A(7, -5, 0); B(1, -1, 9)-> 11.53 */
+
+
+double CalculateDistance(double x1, double y1, double z1, double x2, double y2, double z2)
+{
+   double distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+   return distance;
+}
+Console.WriteLine(CalculateDistance(3, 6, 8, 2, 1, -7));
